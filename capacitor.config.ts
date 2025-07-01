87aed78a707d92097b7f2e80783d72592c8630d4
KumJungMin <rmawjdals@naver.com>
2025-07-01 23:18:44 +0900

    chore: webview 설정 추가

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'ocr-tesseract-playground',
+  webDir: 'dist'
+};
+
+export default config;
